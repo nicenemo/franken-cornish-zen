feeafc328ce760a12d6da1dfc977b988d08fbe2b
Hans Kruse <kruse@hanskruse.eu>
2025-12-08 12:39:03 +0100

    add shifted and unshifted keymap defines for shavian layer

---
+++ config/keys/shavian_manual.h
@@ -0,0 +1,76 @@
+/* shavian_manual_fixed.h – Unicode constants for U+10450 … U+1047F, paired for keyboard layout
+ *
+ * NOTE: Pairs are defined as: [Base (Voiceless/Short/Primary)] [Shift (Voiced/Long/Secondary)]
+ *
+ * This file was generated from Unidata 17.0.0 with the following license:
+ *
+ * UNICODE LICENSE V3
+ *
+ * COPYRIGHT AND PERMISSION NOTICE
+ *
+ * Copyright © 1991-2025 Unicode, Inc.
+ *
+ * NOTICE TO USER: Carefully read the following legal agreement. BY
+ * DOWNLOADING, INSTALLING, COPYING OR OTHERWISE USING DATA FILES, AND/OR
+ * SOFTWARE, YOU UNEQUIVOCALLY ACCEPT, AND AGREE TO BE BOUND BY, ALL OF THE
+ * TERMS AND CONDITIONS OF THIS AGREEMENT. IF YOU DO NOT AGREE, DO NOT
+ * DOWNLOAD, INSTALL, COPY, DISTRIBUTE OR USE THE DATA FILES OR SOFTWARE.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of data files and any associated documentation (the "Data Files") or
+ * software and any associated documentation (the "Software") to deal in the
+ * Data Files or Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, and/or sell
+ * copies of the Data Files or Software, and to permit persons to whom the
+ * Data Files or Software are furnished to do so, provided that either (a)
+ * this copyright and permission notice appear with all copies of the Data
+ * Files or Software, or (b) this copyright and permission notice appear in
+ * associated Documentation.
+ *
+ * THE DATA FILES AND SOFTWARE ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *
+ */
+
+#pragma once
+
+// ----------------------------------------------------------------------
+// 12 Consonant Pairs (8 Voicing Pairs + 4 Other Consonants Paired)
+// ----------------------------------------------------------------------
+// Base (Voiceless/Primary) / Shift (Voiced/Secondary)
+#define UC_SH_PEEP_BIB                           0x10450 0x1045A  // 𐑐 / 𐑚 (P/B)
+#define UC_SH_TOT_DEAD                           0x10451 0x1045B  // 𐑑 / 𐑛 (T/D)
+#define UC_SH_KICK_GAG                           0x10452 0x1045C  // 𐑒 / 𐑜 (K/G)
+#define UC_SH_FEE_VOW                            0x10453 0x1045D  // 𐑓 / 𐑝 (F/V)
+#define UC_SH_THIGH_THEY                         0x10454 0x1045E  // 𐑔 / 𐑞 (Theta/Eth)
+#define UC_SH_SO_ZOO                             0x10455 0x1045F  // 𐑕 / 𐑟 (S/Z)
+#define UC_SH_SURE_MEASURE                       0x10456 0x10460  // 𐑖 / 𐑠 (Sh/Zh)
+#define UC_SH_CHURCH_JUDGE                       0x10457 0x10461  // 𐑗 / 𐑡 (Ch/J)
+
+// Single Consonants Paired for Keyboard Efficiency:
+#define UC_SH_YEA_WOE                            0x10458 0x10462  // 𐑘 / 𐑢 (Y/W)
+#define UC_SH_HUNG_HAHA                          0x10459 0x10463  // 𐑙 / 𐑣 (Eng/H)
+#define UC_SH_LOLL_ROAR                          0x10464 0x1046E  // 𐑤 / 𐑮 (L/R)
+#define UC_SH_MIME_NUN                           0x10465 0x1046F  // 𐑥 / 𐑯 (M/N)
+
+// ----------------------------------------------------------------------
+// 8 Vowel Pairs (Short / Long)
+// ----------------------------------------------------------------------
+// Base (Short) / Shift (Long)
+#define UC_SH_IF_EAT                             0x10466 0x10470  // 𐑦 / 𐑰 (i/ee)
+#define UC_SH_EGG_AGE                            0x10467 0x10471  // 𐑧 / 𐑱 (e/ay)
+#define UC_SH_ASH_ICE                            0x10468 0x10472  // 𐑨 / 𐑲 (a/I)
+#define UC_SH_ADO_AH                             0x10469 0x1046D  // 𐑩 / 𐑭 (ə/a)
+#define UC_SH_ON_AWE                             0x1046A 0x10477  // 𐑪 / 𐑷 (o/aw)
+#define UC_SH_WOOL_OOZE                          0x1046B 0x10475  // 𐑫 / 𐑵 (u/oo)
+#define UC_SH_OUT_OAK                            0x1046C 0x10474  // 𐑬 / 𐑴 (ow/oa)
+#define UC_SH_OIL_ARE                            0x10476 0x10478  // 𐑶 / 𐑸 (oy/ar)
+
+// ----------------------------------------------------------------------
+// 4 R-Controlled Vowel / Diphthong Pairs
+// ----------------------------------------------------------------------
+#define UC_SH_OR_AIR                             0x10479 0x1047A  // 𐑹 / 𐑺 (or/air)
+#define UC_SH_ERR_ARRAY                          0x1047B 0x1047C  // 𐑻 / 𐑼 (er/array)
+#define UC_SH_EAR_IAN                            0x1047D 0x1047E  // 𐑽 / 𐑾 (ear/ian)
+#define UC_SH_YEW                                0x1047F 0        // 𐑿     (yew)
